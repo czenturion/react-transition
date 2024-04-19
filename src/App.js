d291d96f85bb346f62c4486df32cd592e5209860
Maks Arefev <czenturijon@gmail.com>
2024-04-19 13:29:00 +0300

    init: Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="background"></div>
+
+    </div>
+  );
+}
+
+export default App;
